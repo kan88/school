a49c9377c932c6d17b996c0de6e09f1b0acd41d3
Евгений Кан <kan88@csgo.com>
2024-03-01 01:08:16 +0300

    video

--- src/blocks/intro/Intro.jsx
+++ src/blocks/intro/Intro.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import styles from './intro.module.scss'
 import Button from '../../ui/button/Button'
+import video from '../../assets/video/1.mp4'
 
 export default function Intro() {
   return (
     <section className={styles.intro}>
+      <video className={styles.intro__video} autoplay muted loop  controls="controls">
+        <source src={video} type="video/mp4" />
+      </video>
+      <div className={styles.intro__content}>
         <h1 className={styles.intro__title}>Школьные <br/> истории</h1>
         <Button/>
+      </div>
+
     </section>
   )
 }
